d3f3111fbb20b15b108df1a1d096f1e5e47cbc8a
Suryadeep Bhujel <srdpbhujel@gmail.com>
2025-09-27 23:29:15 +0530

    Add BranchType and BranchStatus enums

---
+++ enum/branch-enum.ts
@@ -0,0 +1,12 @@
+export enum BranchType {
+    HEAD_OFFICE = "HEAD_OFFICE",
+    SUBSIDIARY = "SUBSIDIARY",
+    REGIONAL = "REGIONAL",
+    LOCAL = "LOCAL"
+}
+export enum BranchStatus {
+    ACTIVE = "ACTIVE",
+    INACTIVE = "INACTIVE",
+    UNDER_MAINTENANCE = "UNDER_MAINTENANCE",
+    CLOSED = "CLOSED"
+}
