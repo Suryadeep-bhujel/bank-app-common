d3af5b7a8f994f36fb6ad8c743d28105ca807a87
Suryadeep Bhujel <srdpbhujel@gmail.com>
2025-10-05 04:44:58 +0530

    Enhance DateFormatType enum with additional formats and improve date conversion methods

--- service/date-service.ts
+++ service/date-service.ts
@@ -1,7 +1,18 @@
 import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import utc from "dayjs/plugin/utc";
+dayjs.extend(customParseFormat);
+dayjs.extend(utc);
 export enum DateFormatType {
     DD_MM_YYYY = "DD-MM-YYYY",
-    DD_MMM_YYYY = "DD-MMM-YYYY"
+    DD_MMM_YYYY = "DD-MMM-YYYY",
+    DD_MM_YYYY_HH_mm = "DD-MM-YYYY HH:mm",
+    YYYY_MM_DD = "YYYY-MM-DD",
+    YYYY_MM_DD_HH_mm_ss = "YYYY-MM-DD HH:mm:ss",
+    HH_mm_ss = "HH:mm:ss",
+    HH_mm = "HH:mm",
+    YYYY_MM_DDTHH_mm_ssZ = "YYYY-MM-DDTHH:mm:ssZ",// ISO with timezone
+    YYYY_MM_DDTHH_mm_ss_SSSZ = "YYYY-MM-DDTHH:mm:ss.SSSZ"
 }
 export class DateAndTimeService {
     static convertDate(dateString: string, dateFormat: DateFormatType = DateFormatType.DD_MM_YYYY): string {
@@ -12,10 +23,20 @@ export class DateAndTimeService {
         return d.format(dateFormat);
     }
     static convertToDate(date: any, format: DateFormatType = DateFormatType.DD_MM_YYYY): Date {
-        console.log("this.prepareADate(date)", date, this.prepareADate(date, format))
         const datevalue = dayjs(this.prepareADate(date, format), format).toDate();
-        console.log("datevalue convertToDate", datevalue);
         return datevalue;
+    }
+    static convertToDateString(date: any, format: DateFormatType = DateFormatType.DD_MM_YYYY): string {
+        if(!date){
+            return '';
+        }
+        // console.log("date value before conversion", date, "date value after first conversion", this.prepareADate(date, format))
+        const datevalue = dayjs(this.prepareADate(date, format), format).format(format);
+        // console.log("datevalue after final conversion", datevalue);
+        if(datevalue){
+            return datevalue;
+        }
+        return '';
     }
 
     static getCurrentDate(): string {
@@ -43,19 +64,20 @@ export class DateAndTimeService {
             "YYYY-MM-DD",
             "YYYY/MM/DD",
             "YYYY-MM-DDTHH:mm:ssZ", // ISO with timezone
+            "YYYY-MM-DDTHH:mm:ss.SSSZ",
             "MM-DD-YYYY"
         ];
 
         // strict check against known formats (requires customParseFormat plugin to be loaded)
         for (const fmt of formats) {
-            console.log("fmtfmtfmt", fmt, format)
             if (dayjs(date, fmt, true).isValid()) {
-                if (format === fmt) {
+                // console.log("fmtfmtfmt", fmt, format, "date", date)
+                if (format?.toString() === fmt) {
                     return date;
                 }
-                return dayjs(date).format(format);
+                return dayjs(date, fmt).format(String(format));
             }
         }
-        return null;
+        return date;
     }
 }
